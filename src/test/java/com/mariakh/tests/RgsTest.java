579b37088863abe83e4918efac05806227d05ed5
Maria <ronios@yandex.ru>
2023-01-10 21:43:30 +0500

    corrections

--- src/test/java/com/mariakh/tests/RgsTest.java
+++ src/test/java/com/mariakh/tests/RgsTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class RgsTest {
@@ -21,30 +21,24 @@ public class RgsTest {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //ожидание везде, где вызван findByElement
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);//проверяет загружена ли страница, прежде чем искать элемент
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); //ожидание везде, где вызван findByElement
         driver.get("http://www.rgs.ru");
-        wait = new WebDriverWait(driver, 20, 2000);
+        wait = new WebDriverWait(driver, 10, 2000);
     }
 
     @Test
     public void test() {
 
         //Закрыть всплывающее окно с подпиской
-        WebElement iframe = driver.findElement(By.xpath("//iframe[@id = 'fl-616371']"));
-        wait.until(ExpectedConditions.visibilityOf(iframe));
-        driver.switchTo().frame(iframe);
+        driver = wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("fl-616371")));
         WebElement overlayCross = driver.findElement(By.xpath("//div[@data-fl-track = 'click-close-login']"));
         overlayCross.click();
         driver.switchTo().defaultContent();
 
         //Закрыть окно с куки
-        WebElement notificationElement = driver.findElement(By.xpath("//div[@class = 'notifications-block']"));
-        List<WebElement> elements = notificationElement.findElements(By.xpath("./child::*"));
-        if (elements.size() > 1) {
-            WebElement cookieAcceptButton = driver.findElement(By.xpath("//button[@class = 'btn--text']"));
-            cookieAcceptButton.click();
-        }
+        WebElement cookieAcceptButton = driver.findElement(By.xpath("//button[@class = 'btn--text']"));
+        cookieAcceptButton.click();
 
         //Кликнуть на раздел 'Компаниям'
         WebElement companyMenu = driver.findElement(By.xpath("//a[@href = '/for-companies']"));
@@ -80,9 +74,11 @@ public class RgsTest {
         Assert.assertTrue(formToFill.isDisplayed());
 
         //Заполнить имя и проверить, что поле с именем заполнено
-        WebElement nameField = driver.findElement(By.xpath("//input[@name = 'userName']"));
-        nameField.sendKeys("Kotov Oleg Petrovich");
-        Assert.assertEquals("Имена не совпадают", "Kotov Oleg Petrovich", nameField.getAttribute("value"));
+        String fieldXPath = "//input[@name = '%s']";
+        fillInputField(driver.findElement(By.xpath(String.format(fieldXPath, "userName"))), "Kotov Oleg Petrovich");
+
+        //Заполнить адрес электронной почты и проверить, что он введен
+        fillInputField(driver.findElement(By.xpath(String.format(fieldXPath, "userEmail"))), "qwertyqwerty");
 
         //Заполнить номер телефона и проверить, что номер введен
         WebElement phoneField = driver.findElement(By.xpath("//input[@name = 'userTel']"));
@@ -90,35 +86,26 @@ public class RgsTest {
         Assert.assertEquals("Номера телефонов не совпадают", "+7 (999) 888-7766", phoneField.getAttribute("value"));
 
         //Заполнить адрес и проверить, что адрес введен
-        WebElement addressField = driver.findElement(By.xpath("//input[@type = 'text' and contains(@data, vue-dadata)]"));
-        addressField.sendKeys("г Пермь, ул Революции, д 6, кв 71");
+        WebElement addressField = driver.findElement(By.xpath("//input[@type = 'text' and contains(@class, 'vue-dadata')]"));
+        fillInputField(addressField, "г Пермь, ул Революции, д 6, кв 71");
+        WebElement addressToClick = driver.findElement(By.xpath("//span[@class = 'vue-dadata__suggestions-item']"));
+        addressToClick.click();
         Assert.assertEquals("Адреса не совпадают", "г Пермь, ул Революции, д 6, кв 71", addressField.getAttribute("value"));
-
-        //Заполнить адрес электронной почты и проверить, что он введен
-        WebElement emailField = driver.findElement(By.xpath("//input[@name = 'userEmail']"));
-        emailField.sendKeys("qwertyqwerty");
-        Assert.assertEquals("Почты не совпадают", "qwertyqwerty", emailField.getAttribute("value"));
 
         //Кликнуть на чекбокс и проверить, что он не пуст
         WebElement checkboxParent = driver.findElement(By.xpath("//input[@type='checkbox']/.."));
         WebElement checkbox = checkboxParent.findElement(By.xpath("./p/span[2]/span"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", checkbox);
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        scrollToElementJs(checkbox);
         checkbox.click();
         Assert.assertTrue("Пустой чекбокс", checkboxParent.getAttribute("class").contains("is-checked"));
 
+        //Кликнуть на 'Свяжитесь со мной'
         WebElement submitButton = driver.findElement(By.xpath("//button[@type = 'submit']"));
         submitButton.click();
 
-        WebElement wrongEmailMsg = driver.findElement(By.xpath("//span[contains(@class, 'input__error') and text() = " +
-                "'Введите корректный адрес электронной почты']")
-        );
-        Assert.assertEquals("Сообщение об ошибке отсутствует",
+        //Найти сообщение о некорректном адресе электронной почты и проверить его
+        WebElement wrongEmailMsg = driver.findElement(By.xpath("//div[@formkey = 'email']/div/span"));
+        Assert.assertEquals("Сообщение об ошибке отсутствует или отличается от ожидаемого",
                 "Введите корректный адрес электронной почты",
                 wrongEmailMsg.getText()
         );
@@ -133,6 +120,23 @@ public class RgsTest {
     @After
     public void after() {
         driver.quit();
+    }
+
+    private void scrollToElementJs(WebElement element) {
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void fillInputField(WebElement element, String value) {
+        scrollToElementJs(element);
+        element.sendKeys(value);
+        boolean check = wait.until(ExpectedConditions.attributeContains(element, "value", value));
+        Assert.assertTrue("Поле было заполнено некорректно", check);
     }
 
 }
